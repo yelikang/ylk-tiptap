1997147461fbd0a78d7ef7c5e2df2f5766d71f32
yelikang <530331059@qq.com>
2025-05-10 16:28:40 +0800

    增加测试页面

---
+++ demos/src/test/slash/Vue/index.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="container" v-if="editor">
+    <a @click="$_onGetContent">获取内容</a>
+    <editor-content :editor="editor" />
+  </div>
+</template>
+
+<script>
+import Document from "@tiptap/extension-document";
+import Text from '@tiptap/extension-text'
+import Paragraph from '@tiptap/extension-paragraph'
+import { Editor, EditorContent } from "@tiptap/vue-3";
+
+export default {
+  components: {
+    EditorContent,
+  },
+
+  data() {
+    return {
+      editor: null,
+      limit: 280,
+    };
+  },
+
+  mounted() {
+    this.editor = new Editor({
+      extensions: [
+        Document,
+        Paragraph,
+        Text
+      ],
+      content: `
+        <p>
+          Let‘s make sure people can’t write more than 280 characters. I bet you could build one of the biggest social networks on that idea.
+        </p>
+      `,
+    });
+  },
+  methods: {
+    $_onGetContent() {
+      console.log(this.editor.getHTML());
+    },
+  },
+  beforeUnmount() {
+    this.editor.destroy();
+  },
+};
+</script>
+
+<style lang="scss">
+/* Basic editor styles */
+.tiptap {
+  :first-child {
+    margin-top: 0;
+  }
+}
+
+/* Character count */
+.character-count {
+  align-items: center;
+  color: var(--gray-5);
+  display: flex;
+  font-size: 0.75rem;
+  gap: 0.5rem;
+  margin: 1.5rem;
+
+  svg {
+    color: var(--purple);
+  }
+
+  &--warning,
+  &--warning svg {
+    color: var(--red);
+  }
+}
+</style>
