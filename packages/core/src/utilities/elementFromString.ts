4428972d6269b8deddc7e5807542aa6870e98514
yelikang <530331059@qq.com>
2025-05-16 11:02:49 +0800

    string转换为element，并去除空白内容

--- packages/core/src/utilities/elementFromString.ts
+++ packages/core/src/utilities/elementFromString.ts
@@ -4,6 +4,7 @@ const removeWhitespaces = (node: HTMLElement) => {
   for (let i = children.length - 1; i >= 0; i -= 1) {
     const child = children[i]
 
+    // 如果节点是文本节点，(包含换行符加两个空格的节点\n | 只包含换行符的节点\n )，则删除节点
     if (child.nodeType === 3 && child.nodeValue && /^(\n\s\s|\n)$/.test(child.nodeValue)) {
       node.removeChild(child)
     } else if (child.nodeType === 1) {
@@ -14,6 +15,11 @@ const removeWhitespaces = (node: HTMLElement) => {
   return node
 }
 
+/**
+ * 从字符串转换为element元素
+ * @param value 字符串
+ * @returns element元素
+ */
 export function elementFromString(value: string): HTMLElement {
   // add a wrapper to preserve leading and trailing whitespace
   const wrappedValue = `<body>${value}</body>`
